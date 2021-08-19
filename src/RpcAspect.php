6da2d0df7e7d3b590e10a3bee9ccd745abb5bf96
张玉波 <zhangyubo@scjuchuang.com>
2021-08-19 08:55:54 +0800

    优化rpc，支持对象传递

--- src/RpcAspect.php
+++ src/RpcAspect.php
@@ -19,45 +19,8 @@ class RpcAspect implements AroundInterface
 
     public function process(ProceedingJoinPoint $proceedingJoinPoint)
     {
-        if ($proceedingJoinPoint->className == Client::class && $proceedingJoinPoint->methodName === 'send') {
-            return $this->ClientSend($proceedingJoinPoint);
-        } else if ($proceedingJoinPoint->className == SimpleNormalizer::class && $proceedingJoinPoint->methodName === 'denormalize') {
-            $result = $proceedingJoinPoint->process();
-
-            //        foreach ($definitions as $pos => $row) {
-//            if ($row->getName() && strpos($row->getName(), "\\")) {
-//                //是对象
-//                try {
-//                    $arguments[$pos] = arrayToEntity($row->getName(), json_decode(json_encode($arguments[$pos])));
-//                } catch (\Throwable $exception) {
-//                    print_r($exception->getMessage());
-//                    throw $exception;
-//                }
-//
-//            }
-//        }
-            return $result;
+       if ($proceedingJoinPoint->className == ProtocolManager::class && $proceedingJoinPoint->methodName === 'getPacker') {
+            return RpcPacker::class;
         }
-    }
-
-    private function ClientSend(ProceedingJoinPoint $proceedingJoinPoint)
-    {
-        $result = $proceedingJoinPoint->process();
-        //根据协程栈 找到具体调用的rpc方法，检测返回值是否为对象，如果为的对象则进行准换
-        $swooleTrace = \Swoole\Coroutine::getBackTrace(Coroutine::id(), DEBUG_BACKTRACE_IGNORE_ARGS, 15);
-        for ($i = 1; $i < count($swooleTrace); $i++) {
-            $row = $swooleTrace[$i];
-            $ref = new \ReflectionClass($row['class']);
-            if ($ref->isSubclassOf(\Hyperf\RpcClient\Proxy\AbstractProxyService::class)) {
-                $method = $ref->getMethod($row['function']);
-                if ($method->hasReturnType() && strpos($method->getReturnType()->getName(), "Scjc\\Lib\\") === 0) {
-                    $returnType = $method->getReturnType()->getName();
-                    $mapper = new \JsonMapper();
-                    $result['result'] = $mapper->map(json_decode(json_encode($result['result'])), new $returnType);
-                }
-            }
-            unset($ref);
-        }
-        return $result;
     }
 }
---
+++ src/RpcPacker.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Scjc\Interfaces;
+
+
+use Hyperf\Contract\PackerInterface;
+
+/**
+ * 自定义rpc序列化 基于高性能的msgpack
+ * Class RpcPacker
+ * @package Scjc\Interfaces
+ */
+class RpcPacker implements PackerInterface
+{
+    /**
+     * @var string
+     */
+    protected $eof;
+
+    public function __construct(array $options = [])
+    {
+        $this->eof = $options['settings']['package_eof'] ?? "\r\n";
+    }
+
+    public function pack($data): string
+    {
+        return msgpack_pack($data) . $this->eof;
+    }
+
+    public function unpack(string $data)
+    {
+        $data = rtrim($data, $this->eof);
+        return msgpack_unpack($data);
+    }
+}
